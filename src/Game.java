9bcec4c277cd0d36c28c19048f651136632515b5
Ccsar1 <c.valerob.2022@alumnos.urjc.es>
2024-03-19 20:09:39 +0100

    Implementar metodos showWelcome() y initializeDB() de la clase Game

--- src/Game.java
+++ src/Game.java
@@ -1,4 +1,3 @@
-
 import java.io.*;
 import java.util.*;
 
@@ -29,7 +28,7 @@ public class Game {
      * @return
      */
     private boolean login(String nombre, String cont) {
-        // TODO implement here
+        System.out.println("Not Implemented Yet");
         return false;
     }
 
@@ -42,14 +41,69 @@ public class Game {
      */
     private boolean register(String nombre, String nick, String cont, TUser Type) {
         // TODO implement here
+        System.out.println("Not Implemented Yet");
         return false;
     }
 
     /**
      *
      */
-    public void showWelcome() {
+    public void showWelcome() throws IOException {
         // TODO implement here
+        System.out.println("WELCOME TO FIGHT CLUB!!");
+
+        //this.initializeDB();
+        System.out.println("---initializing database");
+
+        int exit = 0;
+
+        while (exit != 1) {
+            exit = 0;
+            System.out.println("1. Log in");
+            System.out.println("2. Register");
+            System.out.println("3. Exit");
+
+            Scanner scanner = new Scanner(System.in);
+            int input = scanner.nextInt();
+
+            switch (input) {
+                case 1:
+                    this.showMenuLogin();
+                    break;
+                case 2:
+                    this.showMenuRegister();
+                    break;
+                case 3:
+                    System.out.println("Please enter 1 to confirm exit");
+                    exit = scanner.nextInt();
+                    if (exit == 1) {
+                        System.out.println("Byebye!");
+                    }
+                    break;
+                default:
+                    System.out.println(input + " is not a valid option");
+            }
+        }
+        //dataBase.saveDB();
+        System.out.println("---saving database");
+    }
+
+    private void initializeDB() throws IOException {
+
+        String filePath = "database.xml";
+
+        File file = new File(filePath);
+
+        if (file.exists()) {
+            try {
+                //this.dataBase = DataBaseManager.loadDB(filePath);
+                System.out.println("---loading database");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            this.dataBase = new DataBaseManager();
+        }
     }
 
     /**
@@ -57,6 +111,7 @@ public class Game {
      */
     private void showMenuLogin() {
         // TODO implement here
+        System.out.println("Not Implemented Yet");
     }
 
     /**
@@ -64,6 +119,7 @@ public class Game {
      */
     private void showMenuRegister() {
         // TODO implement here
+        System.out.println("Not Implemented Yet");
     }
 
     /**
@@ -71,6 +127,7 @@ public class Game {
      */
     private void deleteAccount(User U) {
         // TODO implement here
+        System.out.println("Not Implemented Yet");
     }
 
 }
