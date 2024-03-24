663b085b72d99d99a57dae1727983e5c259f4782
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2024-03-24 17:28:49 +0100

    DataBase functions

--- src/DataBaseManager.java
+++ src/DataBaseManager.java
@@ -3,35 +3,101 @@ import java.io.*;
 import java.util.*;
 
 
-public class DataBaseManager {
+public class DataBaseManager implements Serializable{
 
-    private ArrayList<Character>charDB= new ArrayList<>();
-    private ArrayList<Fight>fightDB= new ArrayList<>();
-    private ArrayList<User>userDB= new ArrayList<>();
-    private ArrayList<Equipment>equipmentDB= new ArrayList<>();
-    private ArrayList<Challenge>challengeDB= new ArrayList<>();
-    private ArrayList<User>userBlockDB= new ArrayList<>();
+    private static final long serialVersionUID = 7608283362899377929L;
+    private ArrayList<Character> charDB = new ArrayList<>();
+    private ArrayList<Fight> fightDB = new ArrayList<>();
+    private ArrayList<User> userDB = new ArrayList<>();
+    private ArrayList<Equipment> equipmentDB = new ArrayList<>();
+    private ArrayList<Challenge> challengeDB = new ArrayList<>();
+    private ArrayList<User> userBlockDB = new ArrayList<>();
 
-    public void setCharDB(Character character) {
+
+    public DataBaseManager() {
+        loadFiles();
+    }
+
+
+    private void loadFiles() {
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("datos2.txt"))) {
+            DataBaseManager savedData = (DataBaseManager) inputStream.readObject();
+            this.userDB = savedData.userDB;
+            this.userBlockDB = savedData.userBlockDB;
+        } catch (FileNotFoundException e) {
+
+            this.userDB = new ArrayList<>();
+            this.userBlockDB = new ArrayList<>();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveFiles() {
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("datos2.txt"))) {
+            outputStream.writeObject(this);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean setCharDB(Character character) {
+        for (Character personaje : charDB) {
+            if (personaje.equals(character)) {
+                return false;
+            }
+        }
         charDB.add(character);
+        saveFiles();
+        return true;
     }
-    public void setFightDB(Fight fight){
+
+    public void setFightDB(Fight fight) {
         fightDB.add(fight);
+        saveFiles();
     }
-    public void setUserDB(User user){
+
+    public void setUserDB(User user) {
+        for (User usuario : userDB) {
+            if (usuario.getNick().equals(user.getNick())) {
+                return;
+            }
+        }
+        for (User userBlock : userBlockDB) {
+            if (userBlock.getNick().equals(user.getNick())) {
+                return;
+            }
+        }
         userDB.add(user);
+        saveFiles();
     }
-    public void setEquipmentDB(Equipment equipment){
+
+    public void getUsersDB() {
+        for (User user : userDB) {
+            System.out.println("Name " + user.getName() + ",nick: " + user.getNick() + ",password: " + user.getPassword() + ",register number: " + user.getRegister_number());
+        }
+    }
+
+    public void getUsersBlockDB() {
+        for (User user : userBlockDB) {
+            System.out.println("Name " + user.getName() + ",nick: " + user.getNick() + ",password: " + user.getPassword() + ",register number: " + user.getRegister_number());
+        }
+    }
+
+    public void setEquipmentDB(Equipment equipment) {
         equipmentDB.add(equipment);
+        saveFiles();
     }
-    public void setChallengeDB(Challenge challenge){
+
+    public void setChallengeDB(Challenge challenge) {
         challengeDB.add(challenge);
+        saveFiles();
     }
 
-    public User getUserByNick(String nick){
-        for(User user:userDB){
+    public User getUserByNick(String nick) {
+        for (User user : userDB) {
 
-            if (user.getNick().equals(nick)){
+            if (user.getNick().equals(nick)) {
                 return user;
             }
 
@@ -40,47 +106,69 @@ public class DataBaseManager {
 
     }
 
-    public String blockUser(String nick){
-        for(User user:userDB){
-            if(user.getNick().equals(nick)){
+
+    public void blockUser(String nick) {
+        for (User user : userDB) {
+            if (user.getNick().equals(nick)) {
                 userDB.remove(user);
                 userBlockDB.add(user);
-                return ("User has been blocked");
+                saveFiles();
+                return;
             }
         }
-        return ("User don´t exist. Try again.");
+
     }
 
-    public String unlockUser(String nick){
-        for(User user:userDB){
-            if(user.getNick().equals(nick)){
+    public void unlockUser(String nick) {
+        for (User user : userBlockDB) {
+            if (user.getNick().equals(nick)) {
                 userBlockDB.remove(user);
                 userDB.add(user);
-                return ("User has been unlocked");
+                saveFiles();
+                return;
             }
         }
-        return ("User don´t exist. Try again.");
+
     }
 
-    public void getRanking(){
+    public void deleteUser(String nick) {
+        Iterator<User> iterator = userDB.iterator();
+        while (iterator.hasNext()) {
+            User user = iterator.next();
+            if (user.getNick().equals(nick)) {
+                iterator.remove();
+            }
+        }
+        Iterator<User> iteratorBlock = userBlockDB.iterator();
+        while (iteratorBlock.hasNext()) {
+            User user = iteratorBlock.next();
+            if (user.getNick().equals(nick)) {
+                iteratorBlock.remove();
+            }
+        }
+        saveFiles();
+    }
+
+    public void getRanking() {
         Collections.sort(charDB, new Comparator<Character>() {
             @Override
             public int compare(Character o1, Character o2) {
-                return Integer.compare(o1.getGold(),o2.getGold());
+                return Integer.compare(o1.getGold(), o2.getGold());
             }
         });
-        }
+    }
 
-    public String deleteCharacter (String name){
-        for (Character character:charDB){
-            if(character.getName().equals(name)){
+    public void deleteCharacter(String name) {
+        for (Character character : charDB) {
+            if (character.getName().equals(name)) {
                 charDB.remove(character);
-                return ("Character deleted");
+                saveFiles();
+                return;
             }
+
         }
-        return ("Character not found. Try again");
-    }
 
     }
+}
 
 
