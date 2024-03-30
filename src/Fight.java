8e7b8a0ba84470463884fa205ecd06055f098a1e
AlbertoML1999 <a.manjon.2022@alumnos.urjc.es>
2024-03-30 18:20:46 +0100

    Fight y round empezando

--- src/Fight.java
+++ src/Fight.java
@@ -2,9 +2,7 @@
 import java.io.*;
 import java.util.*;
 
-/**
- *
- */
+
 public class Fight {
 
     private CharacterUser characterChallenger;
@@ -12,19 +10,84 @@ public class Fight {
     private int gold;
     private int roundNum;
     private ArrayList<Round>roundList;
-    private TResult state;
-    public Fight() {
+    private int result;
+    private boolean notified;
+    public Fight(CharacterUser characterChallenger, CharacterUser characterChallenged, int gold ) {
+        this.characterChallenger=characterChallenger;
+        this.characterChallenged=characterChallenged;
+        this.gold=gold;
+    }
+    //esto se mete en startRound
+    //1.llamo calculateAttack y calculateDefense de ambos characterUser
+    //2.Hacer lo de los numeros aleatorios que ponen en el enunciado de la practica
+    //3.doDamage 1, si el ataque sea mayor que la defensa
+    //esto en startfight
+    //meter en arraylist round
+    //4.compruebo si tienen salud, hago otra ronda
+
+    //en showresult, imprimo el arraylist de las rondas, pongo quien ha ganado al final, y quito el oro en la función que me ha puesto Cesar en Whatsapp; y si la lista esta vacia, pongo que el challenged ha rechazado el desafío
+
+    public void giveUp(){
+
     }
 
     public void startFight() {
-        // TODO implement here
+        int HPChallenger,HPChallenged;
+        Round round= new Round(characterChallenger,characterChallenged);
+
+do {
+    round.playRound();
+    roundList.add(round);
+    HPChallenger=getChallenger().getHP();
+    HPChallenged=getChallenged().getHP();
+}while(HPChallenger>0 && HPChallenged>0);
+
+
     }
 
-    /**
-     *
-     */
-    public void showResult() {
-        // TODO implement here
+
+    public void showResult(TResult state, CharacterUser character) {
+        System.out.println("FINISH!!");
+
+        switch (state){
+            case Win:
+                System.out.println(character+" you win the fight!");
+                System.out.println((gold*2)+" earned, congratulations!");
+                int goldTotal= character.getGold();
+                character.setGold(goldTotal+gold);
+                break;
+            case Draw:
+                System.out.println(character+" you draw the fight!");
+                break;
+            case Lose:
+                System.out.println(character+" you lose the fight!");
+                System.out.println((gold)+" remove from your account, sorry!");
+                int goldT= character.getGold();
+                character.setGold(goldT-gold);
+                break;
+        }
+
+    }
+
+    public boolean getNotified(){
+        return notified;
+    }
+    public void setNotified(){
+        this.notified=true;
+
+    }
+
+    public CharacterUser getChallenger(){
+        return this.characterChallenger;
+    }
+    public CharacterUser getChallenged(){
+        return this.characterChallenged;
+    }
+    public void setChallenger(CharacterUser character){
+        this.characterChallenger=character;
+    }
+    public void setChallenged(CharacterUser character){
+        this.characterChallenged=character;
     }
 
 }
--- src/Round.java
+++ src/Round.java
@@ -7,37 +7,61 @@ import java.util.*;
  */
 public class Round {
 
-    /**
-     * Default constructor
-     */
-    public Round() {
+    Random random=new Random();
+    CharacterUser characterChallenger;
+    CharacterUser characterChallenged;
+
+
+    public Round(CharacterUser characterChallenger, CharacterUser characterChallenged) {
+        this.characterChallenger=characterChallenger;
+        this.characterChallenged=characterChallenged;
+
     }
 
-    /**
-     *
-     */
-    private CharacterUser attacker;
-
-    /**
-     *
-     */
-    private CharacterUser defender;
-
-    /**
-     *
-     */
-    private int damage;
-
-    /**
-     *
-     */
     public void playRound() {
-        // TODO implement here
+        int attackChallengerExists=0,defenceChallengerExists=0,attackChallengedExists=0,defenceChallengedExists=0;
+        int potentialAttackCharacterChallenger= characterChallenger.calculateAttack();
+        int potentialDefenceCharacterChallenger= characterChallenger.calculateDefense();
+        int potentialAttackCharacterChallenged= characterChallenger.calculateAttack();
+        int potentialDefenceCharacterChallenged= characterChallenger.calculateDefense();
+        int [] attackChallenger= new int[potentialAttackCharacterChallenger];
+        int [] defenceChallenger= new int[potentialDefenceCharacterChallenger];
+        int [] attackChallenged= new int[potentialAttackCharacterChallenged];
+        int [] defenceChallenged= new int[potentialDefenceCharacterChallenged];
+
+        for (int i=0; i<potentialAttackCharacterChallenger; i++){
+            int randomNumber= random.nextInt(6)+1;
+            attackChallenger[i]=randomNumber;
+            if (randomNumber==5 || randomNumber==6){
+                attackChallengerExists++;
+            }
+        }
+        for (int i=0; i<potentialDefenceCharacterChallenger; i++){
+            int randomNumber= random.nextInt(6)+1;
+            defenceChallenger[i]=randomNumber;
+            if (randomNumber==5 || randomNumber==6){
+                defenceChallengerExists++;
+            }
+        }
+        for (int i=0; i<potentialAttackCharacterChallenged; i++){
+            int randomNumber= random.nextInt(6)+1;
+            attackChallenged[i]=randomNumber;
+            if (randomNumber==5 || randomNumber==6){
+                attackChallengedExists++;
+            }
+        }
+        for (int i=0; i<potentialDefenceCharacterChallenged; i++){
+            int randomNumber= random.nextInt(6)+1;
+            defenceChallenged[i]=randomNumber;
+            if (randomNumber==5 || randomNumber==6){
+                defenceChallengedExists++;
+            }
+        }
+
+
     }
 
-    /**
-     *
-     */
+
     public void showRound() {
         // TODO implement here
     }
